09204261d8e017f3a2bd5bb413bb40be5023fa69
Matthew Osborn <matthew@osbornm.com>
2024-01-27 10:45:46 -0600

    feat: add heading component

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,10 @@
-/** @type {import('jest').Config} */
-const config = {
+module.exports = {
   moduleNameMapper: {
     // Mocks out all these file formats when tests are run
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       'identity-obj-proxy',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-  }
+  },
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  restoreMocks: true,
 };
-
-module.exports = config;
