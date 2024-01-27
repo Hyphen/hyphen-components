510f92a0095a4e8659f07db0673832f7663d9606
Matthew Osborn <matthew@osbornm.com>
2024-01-27 17:07:06 -0600

    feat: Add Button, Icon, and Spinner (#3)
    * feat: Supporting components
    
    * feat: add button component

---
+++ src/components/Icon/Icon.stories.tsx
@@ -0,0 +1,71 @@
+import { Box } from '../Box/Box';
+import { ICON_NAMES } from '../../lib/tokens';
+import { Icon } from './Icon';
+import type { Meta } from '@storybook/react';
+import React from 'react';
+
+const meta: Meta<typeof Icon> = {
+  title: 'Components/Icon',
+  component: Icon,
+};
+
+export default meta;
+
+export const Default = () => <Icon name="star" />;
+
+export const Sizes = () => (
+  <>
+    <Box direction="row" childGap="sm" margin="0 0 md 0" fontSize="xs">
+      <Icon name="remove" color="danger-500" />
+      <Box>Size is set by the Icon's parent</Box>
+    </Box>
+    <Box direction="row" fontSize="xl" childGap="sm">
+      <Icon name="star" color="warning-400" />
+      <Box>Size is set by the Icon's parent</Box>
+    </Box>
+  </>
+);
+
+export const Colors = () => (
+  <>
+    <Box direction="row" childGap="sm" margin="0 0 md 0">
+      <Icon name="remove" color="danger-500" />
+      <Box>Color is set at the Icon level</Box>
+    </Box>
+    <Box direction="row" childGap="sm" color="secondary-400">
+      <Icon name="book" />
+      <Box>Color is set by the Icon's parent</Box>
+    </Box>
+  </>
+);
+
+export const UnknownIcon = () => (
+  <Icon name={'does-not-exist' as any} className="font-size-2xl" />
+);
+
+export const AvailableIcons = () => (
+  <Box
+    style={{
+      display: 'grid',
+      gridTemplateColumns: 'repeat(auto-fill,minmax(6rem,1fr))',
+      gridAutoRows: '6rem',
+      gap: '1rem',
+    }}
+  >
+    {ICON_NAMES.map((name) => (
+      <Box
+        key={name}
+        fontSize="lg"
+        textAlign="center"
+        alignItems="center"
+        justifyContent="center"
+        childGap="sm"
+      >
+        <Icon className="neutral-500" name={name} />
+        <Box as="p" color="grey-500" fontSize="sm">
+          {name}
+        </Box>
+      </Box>
+    ))}
+  </Box>
+);
---
+++ src/components/Spinner/Spinner.stories.tsx
@@ -0,0 +1,46 @@
+import { BRAND_COLOR_NAMES } from '../../lib/tokens';
+import { Box } from '../Box/Box';
+import type { Meta } from '@storybook/react';
+import React from 'react';
+import { Spinner } from './Spinner';
+
+const meta: Meta<typeof Spinner> = {
+  title: 'Components/Spinner',
+  component: Spinner,
+};
+
+export default meta;
+
+export const Default = () => <Spinner />;
+
+export const Variants = () => (
+  <Box display="block" childGap="md">
+    {BRAND_COLOR_NAMES.map((color) => (
+      <Box direction="row" childGap="sm">
+        <Spinner variant={color} />
+        <div>{color}</div>
+      </Box>
+    ))}
+  </Box>
+);
+
+export const Sizes = () => (
+  <Box direction="row" childGap="md">
+    <Box direction="row" childGap="sm">
+      <Spinner size="sm" />
+      <div>sm</div>
+    </Box>
+    <Box direction="row" childGap="sm">
+      <Spinner size="md" />
+      <div>md</div>
+    </Box>
+    <Box direction="row" childGap="sm">
+      <Spinner size="lg" />
+      <div>lg</div>
+    </Box>
+    <Box direction="row" childGap="sm">
+      <Spinner size="xl" />
+      <div>xl</div>
+    </Box>
+  </Box>
+);
