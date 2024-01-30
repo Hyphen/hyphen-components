f56e669382c3ed5cd4102da68833c524909ecd4f
Charles Sullivan <charlie.sullivan@gmail.com>
2024-01-30 15:15:59 -0700

    More lint fixes

--- src/components/Alert/Alert.stories.tsx
+++ src/components/Alert/Alert.stories.tsx
@@ -97,56 +97,53 @@ export const RenderProp = () => (
   </>
 );
 
-export const Closable = () => (
-  <>
-    {(() => {
-      const [isAlertTwoShowing, setAlertTwoShowing] = useState(true);
-      const [isAlertThreeShowing, setAlertThreeShowing] = useState(true);
-      return (
-        <>
-          <Alert
-            title="Won't Close"
-            message="Closable, but with no onClose callback so nothing happens when clicked."
-            variant="warning"
-            isClosable
-            className="m-bottom-md"
-          />
-          {isAlertTwoShowing ? (
-            <Alert
-              title="Closable"
-              message="This one works!"
-              variant="info"
-              isClosable
-              onClose={() => setAlertTwoShowing(false)}
-              className="m-bottom-md"
-            />
-          ) : (
-            <div className="m-bottom-md">
-              <Button onClick={() => setAlertTwoShowing(true)} size="sm">
-                Give me the second alert back!
-              </Button>
-            </div>
-          )}
-          {isAlertThreeShowing ? (
-            <Alert
-              message="With custom close text!"
-              variant="info"
-              isClosable
-              onClose={() => setAlertThreeShowing(false)}
-              closeText="Close me!"
-            />
-          ) : (
-            <div className="m-bottom-md">
-              <Button onClick={() => setAlertThreeShowing(true)} size="sm">
-                Give me the third alert back!
-              </Button>
-            </div>
-          )}
-        </>
-      );
-    })()}
-  </>
-);
+export const Closable = () => {
+  const [isAlertTwoShowing, setAlertTwoShowing] = useState(true);
+  const [isAlertThreeShowing, setAlertThreeShowing] = useState(true);
+
+  return (
+    <>
+      <Alert
+        title="Won't Close"
+        message="Closable, but with no onClose callback so nothing happens when clicked."
+        variant="warning"
+        isClosable
+        className="m-bottom-md"
+      />
+      {isAlertTwoShowing ? (
+        <Alert
+          title="Closable"
+          message="This one works!"
+          variant="info"
+          isClosable
+          onClose={() => setAlertTwoShowing(false)}
+          className="m-bottom-md"
+        />
+      ) : (
+        <div className="m-bottom-md">
+          <Button onClick={() => setAlertTwoShowing(true)} size="sm">
+            Give me the second alert back!
+          </Button>
+        </div>
+      )}
+      {isAlertThreeShowing ? (
+        <Alert
+          message="With custom close text!"
+          variant="info"
+          isClosable
+          onClose={() => setAlertThreeShowing(false)}
+          closeText="Close me!"
+        />
+      ) : (
+        <div className="m-bottom-md">
+          <Button onClick={() => setAlertThreeShowing(true)} size="sm">
+            Give me the third alert back!
+          </Button>
+        </div>
+      )}
+    </>
+  );
+};
 
 export const Compact = () => (
   <Alert message="Compact Alerts have less padding" variant="info" isCompact />
