73d8ff13efa8804935327172c94907bc740b13bf
Charles Sullivan <charlie.sullivan@gmail.com>
2024-01-31 16:18:59 -0700

    Addtional Card Examples

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -239,3 +239,61 @@ export const ExampleCardWithTable = () => {
     </Card>
   );
 };
+
+export const ExampleFullBleedPhotos = () => (
+  <Box
+    alignItems="center"
+    alignContent="center"
+    padding="2xl"
+    background="grey-lightest"
+  >
+    <Card width="300px">
+      <Card.Section padding="0" overflow="hidden" height="300px">
+        <img
+          src="https://images.unsplash.com/photo-1555412654-72a95a495858?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=80"
+          data-chromatic="ignore"
+          alt=""
+        />
+      </Card.Section>
+      <Card.Section childGap="xs">
+        <Heading>Headline</Heading>
+        <Box as="p">This is the body copy of the card.</Box>
+      </Card.Section>
+    </Card>
+  </Box>
+);
+
+export const ExampleSingleInstancePhotos = () => (
+  <Box gap="xl">
+    <Card>
+      <Card.Section>Default appearance</Card.Section>
+      <Card.Section>Default appearance</Card.Section>
+      <Card.Footer>footer</Card.Footer>
+    </Card>
+    <Box
+      gap="xl"
+      style={{
+        '--card-background-color': 'var(--color-brand-tertiary-50)',
+        '--card-footer-background-color': 'var(--color-brand-tertiary-900)',
+        '--card-border-radius': 'var(--size-border-radius-lg)',
+        '--card-section-border-color': 'var(--color-brand-tertiary-100)',
+        '--card-box-shadow': '0',
+      }}
+    >
+      <Card>
+        <Card.Section>Themed card using component design tokens</Card.Section>
+        <Card.Section>Themed card using component design tokens</Card.Section>
+        <Card.Footer color="white">footer</Card.Footer>
+      </Card>
+      <Card shadow="xl" radius="sm" background="secondary-50">
+        <Card.Section>BoxProps are used instead of tokens</Card.Section>
+        <Card.Section borderWidth="sm 0 0 0" borderColor="secondary-100">
+          BoxProps are used instead of tokens
+        </Card.Section>
+        <Card.Footer background="secondary-900" color="grey-100">
+          footer
+        </Card.Footer>
+      </Card>
+    </Box>
+  </Box>
+);
