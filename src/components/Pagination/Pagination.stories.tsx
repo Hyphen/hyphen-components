854c4805886b082aa7931396b6cff99822bae231
Charles Sullivan <charlie.sullivan@gmail.com>
2024-01-31 10:49:12 -0700

    Add Pagination Component

---
+++ src/components/Pagination/Pagination.stories.tsx
@@ -0,0 +1,55 @@
+import { Pagination } from './Pagination';
+import type { Meta } from '@storybook/react';
+import React from 'react';
+import { useState } from 'react';
+
+const meta: Meta<typeof Pagination> = {
+  title: 'Components/Pagination',
+  component: Pagination,
+};
+
+export default meta;
+
+export const Default = () => {
+  const [activePage, setActivePage] = useState(1);
+
+  return (
+    <>
+      <Pagination
+        activePage={activePage}
+        itemsPerPage={20}
+        onChange={setActivePage}
+        totalItemsCount={87}
+        numberOfPagesDisplayed={0}
+      />
+    </>
+  );
+};
+
+export const Compact = () => {
+  const [activePage, setActivePage] = useState(1);
+  return (
+    <Pagination
+      activePage={activePage}
+      itemsPerPage={20}
+      onChange={setActivePage}
+      totalItemsCount={100}
+      isCompact
+    />
+  );
+};
+
+export const PageNumbers = () => {
+  const [activePage, setActivePage] = useState(1);
+  return (
+    <Pagination
+      activePage={activePage}
+      itemsPerPage={20}
+      onChange={setActivePage}
+      totalItemsCount={1000}
+      isCompact
+      arePagesVisible
+      numberOfPagesDisplayed={3}
+    />
+  );
+};
--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export * from './components/Box/Box';
 export * from './components/Button/Button';
 export * from './components/Heading/Heading';
 export * from './components/Icon/Icon';
+export * from './components/Pagination/Pagination';
 export * from './components/Spinner/Spinner';
