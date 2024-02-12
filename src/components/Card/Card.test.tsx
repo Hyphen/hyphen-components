42f23e4d17e4565c46d520b69ad2c5fdc450c535
Charles Sullivan <charlie.sullivan@gmail.com>
2024-02-12 14:23:23 -0700

    feat: Card Component and related components

---
+++ src/components/Card/Card.test.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/no-array-index-key */
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Card } from './Card';
+
+describe('Card', () => {
+  test('card has overflow hidden by default', () => {
+    const { container } = render(<Card>test</Card>);
+    expect(container.children[0].classList).toContain('overflow-hidden');
+  });
+});
