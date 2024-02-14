252ec7099b5c02c38502ae05cc2718b6d0970d36
Charles Sullivan <charlie.sullivan@gmail.com>
2024-02-14 07:28:40 -0700

    feat: Add FormLabel docs

---
+++ src/components/FormLabel/FormLabel.stories.tsx
@@ -0,0 +1,35 @@
+import { FormLabel } from './FormLabel';
+
+import type { Meta } from '@storybook/react';
+import React from 'react';
+
+const meta: Meta<typeof FormLabel> = {
+  title: 'Components/FormLabel',
+  component: FormLabel,
+  parameters: {
+    controls: { hideNoControlsWarning: true },
+  },
+};
+
+export default meta;
+
+export const DefaultFormLabel = () => (
+  <FormLabel inputId="inputId">Default Label</FormLabel>
+);
+
+export const InteractiveFormLabel = () => (
+  <FormLabel inputId="customText">
+    This label has a span with
+    <span style={{ fontStyle: 'italic' }}>&nbsp;Italic&nbsp;</span>
+    and also a&nbsp;
+    <a href="https://hyphen.ai" target="_blank" rel="noreferrer">
+      Link
+    </a>
+  </FormLabel>
+);
+
+export const HelpTextFormLabel = () => (
+  <FormLabel inputId="required" helpText="More helpful text about the input">
+    Required input label
+  </FormLabel>
+);
