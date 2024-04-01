3334f5e0c47814846b28db148f5e22ff87894897
cialese <cinthyaless@gmail.com>
2024-04-01 11:34:37 -0500

    fix syntax

--- src/components/Toast/toast.ts
+++ src/components/Toast/toast.ts
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { resolveValue } from '../../lib/resolveValue';
 import { ValueOrFunction } from '../../types';
