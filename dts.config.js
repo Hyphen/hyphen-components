f1f5ca81fc6fa387269f7d4ccaa567cf0029edcc
Christian Llontop <chrisllontop@icloud.com>
2024-04-25 10:18:12 -0500

    Add use client directive

--- dts.config.js
+++ dts.config.js
@@ -4,24 +4,29 @@ const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 
 module.exports = {
-  rollup(config, options) {
-    config.plugins.push(
-      postcss({
-        plugins: [
-          autoprefixer(),
-          cssnano({
-            preset: 'default',
-          }),
-        ],
-        use: [
-          ['sass', {
-            includePaths: [path.resolve('node_modules')]
-          }]
-        ],
-        inject: false,
-        extract: path.resolve('dist/css/index.css'),
-      })
-    );
-    return config;
-  },
+    rollup(config, options) {
+        config.plugins.push(
+            postcss({
+                plugins: [
+                    autoprefixer(),
+                    cssnano({
+                        preset: 'default',
+                    }),
+                ],
+                use: [
+                    ['sass', {
+                        includePaths: [path.resolve('node_modules')]
+                    }]
+                ],
+                inject: false,
+                extract: path.resolve('dist/css/index.css'),
+            })
+        );
+
+        if (!config.output.banner) {
+            config.output.banner = "'use client';";
+        }
+
+        return config;
+    },
 };
