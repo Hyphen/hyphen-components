8f5c214354e096ed9c3d560aa978106414a43fda
Nathan Young <1447339+nathanyoung@users.noreply.github.com>
2024-08-23 17:40:51 -0700

    feat: add isDarkMode boolean to useTheme hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,5 @@
 export * from './useBreakpoint/useBreakpoint';
-export * from './useOpenClose/useOpenClose';
-export * from './useWindowSize/useWindowSize';
 export * from './useIsomorphicLayoutEffect/useIsomorphicLayouEffect';
+export * from './useOpenClose/useOpenClose';
+export * from './useTheme/useTheme';
+export * from './useWindowSize/useWindowSize';
